8fbf1571124f94e99f4e6aec213d567dda349acd
Mitchel Parish <mitchelparish@gmail.com>
2019-01-03 19:04:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(integer)
-    return false if integer < 2
-    (2..integer - 1).each do |x|
-        if (integer % x) == 0
-            return false
-        end
+  if number >= 2
+    (2..number - 1).each do |x|
+      number % x != 0
     end
-    true
+  else
+    return false
+  end
 end
